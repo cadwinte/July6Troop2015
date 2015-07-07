c755acb360b71bdc1689415b172ab3d32bb0806d
cadwinte <cadeawinter@gmail.com>
2015-07-07 11:51:53 -0500

    Update

--- Day 1 HomeWork/Day 1 HomeWork/Program.cs
+++ Day 1 HomeWork/Day 1 HomeWork/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,21 @@ namespace Day_1_HomeWork
     {
         static void Main(string[] args)
         {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+                
+            player.SoundLocation = "http://www.wavsource.com/snds_2015-07-05_1837925197659629/movies/cool_hand_luke/failure2_x.wav";
+            
+
+            player.SoundLocation = "http://www.wavsource.com/snds_2015-07-05_1837925197659629/movie_stars/misc/gibson_braveheart_freedom.wav";
+            
+
+            player.SoundLocation = "http://www.wavsource.com/snds_2015-07-05_1837925197659629/movies/misc/bring_it_on_x.wav";
+            
+
+            Random rand = new Random();
+            int taco = rand.Next(3);
+
+
         }
     }
 }
